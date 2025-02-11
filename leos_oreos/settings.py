0eccefe971703c99540982f4f0bc5bcd7bd87ace
neil7057 <neil7057@gmail.com>
2025-02-11 13:47:31 +0000

    add product categories

--- leos_oreos/settings.py
+++ leos_oreos/settings.py
@@ -91,7 +91,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_LOGIN_METHODs = {'username_email'}
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
