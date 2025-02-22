66077d63ce5b7ed44d8d8bdfeb399aa5f87c2289
neil7057 <neil7057@gmail.com>
2025-02-22 13:38:45 +0000

    add about and privacy pages and links

--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def index(request):
     """ view to return index page """
     
     return render(request, 'home/index.html')
+
+def about(request):
+    """ view to return about us page """
+    
+    return render(request, 'home/about.html')
+
+def privacy(request):
+    """ view to return privacy notice page """
+    
+    return render(request, 'home/privacy.html')
