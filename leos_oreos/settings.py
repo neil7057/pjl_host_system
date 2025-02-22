ccb8c5df864c409572ec8f41209a84ca19f45349
neil7057 <neil7057@gmail.com>
2025-02-22 21:38:32 +0000

    add static root command to settings

--- leos_oreos/settings.py
+++ leos_oreos/settings.py
@@ -188,10 +188,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'),)
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 if 'USE_AWS' in os.environ:
     # AWS Bucket settings
