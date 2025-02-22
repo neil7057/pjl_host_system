cd135c2f514c79917ee4531c7818643807cde403
neil7057 <neil7057@gmail.com>
2025-02-22 18:45:26 +0000

    remove debug settings

--- leos_oreos/settings.py
+++ leos_oreos/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
                 '127.0.0.1',  # vs code preview
