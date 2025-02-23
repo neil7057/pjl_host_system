cbeb970f8e9c949412aaa1d325fab4097a499a21
neil7057 <neil7057@gmail.com>
2025-02-23 18:03:13 +0000

    add AWS cacge settings

--- leos_oreos/settings.py
+++ leos_oreos/settings.py
@@ -187,13 +187,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # AWS Bucket settings
     AWS_STORAGE_BUCKET_NAME = 'leos-oreos'
     AWS_S3_REGION_NAME = 'eu-north-1'
