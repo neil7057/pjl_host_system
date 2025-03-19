74bddca78922efb7365b703ef28c3d1567b60d03
neil7057 <neil7057@gmail.com>
2025-03-19 21:42:10 +0000

    add search results page, multi choice category add on host

--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ def index(request):
     """ view to return index page """
 
     return render(request, 'home/index.html')
+
+def search(request):
+    """ view to return search results page """
+
+    return render(request, 'home/search_results.html')
--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,14 @@
 // select 2 for multiselect - select2.org/getting-started/basic-usage
 // Initialize select2
 
+// $(document).ready(function() {
+//   $('#id_categories').select2();
+// });
+
 $(document).ready(function () {
-    $('#thread_tags').select2({
-      placeholder: "Select Tags",
-      tags: true,
-      tokenSeparators: [',', ' '],
-      // apply selection limit with Select2 (https://select2.org/selections)
-      maximumSelectionLength: 4,
-    });
+  $('#id_categories').select2({
+    placeholder: "Select Category",
+    tags: true,
+    tokenSeparators: [',', ' '],
   });
+});
